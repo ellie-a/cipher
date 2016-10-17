39470256995e825ca35a95aae7db80e6ad9cae9e
Ellie Angell and Ezra Sandzer-Bell <ellie@>
2016-10-17 15:07:36 -0700

    reverse the capitalized first and last letter and add it to the original word

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@
 var yourWord = prompt("Enter a sentence.");
 var result = yourWord.charAt(0) + yourWord.charAt(yourWord.length-1);
 
-var combinedResult = result.toUpperCase();
-alert(combinedResult);
+var combinedResult = result.toUpperCase().split('').reverse('').join('');
+
+alert(combinedResult + yourWord);
