8174374d510f148636275b8a2e45bc437c38d21e
Ellie Angell and Ezra Sandzer-Bell <ezra@>
2016-10-17 15:45:00 -0700

    add additional functions that count the number of letter in original sentence, divide by two, and output the middle letter

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ var yourWord = prompt("Enter a sentence.");
 var result = yourWord.charAt(0) + yourWord.charAt(yourWord.length-1);
 
 var combinedResult = result.toUpperCase().split('').reverse('').join('');
+var letterCount = yourWord.length;
+var half = (letterCount / 2)
+var midLetter = yourWord.charAt(half);
 
-alert(combinedResult + yourWord);
+var modifyReverse = midLetter + yourWord + combinedResult
+var finalOutput = modifyReverse.split('').reverse('').join('');
+
+alert(finalOutput);
